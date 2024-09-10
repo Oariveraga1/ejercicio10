e0849bda955cb9978c48af180a0397fe5fa866a3
Oscar Rivera <oscar.0993@gmail.com>
2024-09-09 23:46:41 -0500

    abance de codigo

--- EJERCICIO10NUEVO/Program.cs
+++ EJERCICIO10NUEVO/Program.cs
@@ -1,7 +1,29 @@
 ﻿public class Program
 {
+    
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //construir un algoritmo que pida por pantalla 3 numeros y
+        //despues diga cual es el mayor, el del medio y el numero menor
+        //de los numeros ingresados
+
+        double nume1, nume2, nume3;
+
+        //solicitar los numeros al cliente
+        Console.Write("por favor escribe el primer número: ");
+        nume1 = Convert.ToDouble(Console.ReadLine());
+       
+        //el conver.ToDouble se escribe para convertir el tipo de texto
+
+        Console.Write("por favor escribe el segundo número: ");
+        nume1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("por favor escribe el tercer número: ");
+        nume1 = Convert.ToDouble(Console.ReadLine());
+
+
+
     }
 }
